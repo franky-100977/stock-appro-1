a52e2251286685fc051bd57dc566c39139ee6d8a
vporel <76995942+vporel@users.noreply.github.com>
2021-03-14 19:28:40 +0100

    rien

--- model/classes/DateSys.class.php
+++ model/classes/DateSys.class.php
@@ -1,76 +1,76 @@
 <?php 
 class DateSys{
-	protected $jour, $mois, $annee, $time_date;
-	public static $mois_array = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+	protected $day, $month, $year, $time_date;
+	public static $MONTHS = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+	public static $SHORT = "s", $LONG = "l";
 
-	public function __construct($jour, $mois = 0, $annee = 0){
-		if($mois == 0 AND $annee == 0){
-			$this->setDate($jour);
-		}elseif($mois != 0 AND $annee == 0){
+	public function __construct($day, $month = 0, $year = 0){
+		if($month == 0 AND $year == 0){
+			$this->setDate($day);
+		}elseif($month != 0 AND $year == 0){
 			trigger_error("DateSys::__construct : Erreur lors d'instanciation: renseignez l'année", E_USER_WARNING);
 		}else{
-			$this->setAnnee($annee);
-			$this->setMois($mois);
-			$this->setJour($jour);
+			$this->setyear($year);
+			$this->setmonth($month);
+			$this->setday($day);
 		}
-		$this->time_date = mktime(0,0,0, $this->mois, $this->jour, $this->annee);
+		$this->time_date = mktime(0,0,0, $this->month, $this->day, $this->year);
 	}
 	// GETTERS
 	public function getTime_date(){return $this->time_date;}
-	public function getJour($param = ""){
-		if($param == "l")
+	public function getDay($param = ""){
+		if($param == self::$LONG)
 			return date('D', $this->time_date);
-		elseif($param == "num"){
+		elseif($param == self::$SHORT){
 			$num = date('w', $this->time_date);
 			if($num == 0) $num = 7;
 			return $num;
 		}
-		return $this->jour;
+		return $this->day;
 	}
-	public function getMois($param = ""){
-		$mois_array = self::$mois_array;
+	public function getMonth($param = ""){
 		if($param == "l")
-			return $mois_array[$this->mois-1];
-		return $this->mois;
+			return self::$MONTHS[$this->month-1];
+		return $this->month;
 	}
-	public function getAnnee(){return $this->annee;}
+	public function getYear(){return $this->year;}
 	//SETTERS
-	public function setJour($jour){
-		if(is_int($jour) AND $jour > 0 AND $jour <=31){
-			if($this->mois == 2){
-				if($jour <= 29){
-					$this->jour = $jour;
+	public function setDay($day){
+		if(is_int($day) AND $day > 0 AND $day <=31){
+			if($this->month == 2){
+				if($day <= 29){
+					$this->day = $day;
 				}else{
-				trigger_error("Le mois dans cette date est le deuxieme donc on ne peut avoir un nombre de jours supérieur à 29",E_USER_WARNING);
+				trigger_error("Le month dans cette date est le deuxieme donc on ne peut avoir un nombre de days supérieur à 29",E_USER_WARNING);
 				}
-			}else if(in_array($this->mois, array(4,6,9,11))){
-				if($jour <= 30){
-					$this->jour = $jour;
+			}else if(in_array($this->month, array(4,6,9,11))){
+				if($day <= 30){
+					$this->day = $day;
 				}else{
-				trigger_error("Le mois dans cette date ne peut avoir un nombre de jours supérieur à 30",E_USER_WARNING);
+				trigger_error("Le month dans cette date ne peut avoir un nombre de days supérieur à 30",E_USER_WARNING);
 				}
 			}else{
-				$this->jour = $jour;
+				$this->day = $day;
 			}
 		}else{
-			trigger_error("Le jour dans la date doit etre un entier strictement positif inférieur ou égal à 31", E_USER_WARNING);
+			trigger_error("Le day dans la date doit etre un entier strictement positif inférieur ou égal à 31", E_USER_WARNING);
 		}
 	}
-	public function setMois($mois){
-		if(is_int($mois) AND $mois > 0 AND $mois <=12){
-			if(($mois == 2 AND $this->jour > 29) OR(in_array($mois, array(4,6,9,11)) AND $this->jour > 30)){
-				trigger_error("Cette date a ".$this->jour." jours. Vous ne pouvez donc pas mettre le mois N° ".$mois, E_USER_WARNING);
+	public function setmonth($month){
+		if(is_int($month) AND $month > 0 AND $month <=12){
+			if(($month == 2 AND $this->day > 29) OR(in_array($month, array(4,6,9,11)) AND $this->day > 30)){
+				trigger_error("Cette date a ".$this->day." days. Vous ne pouvez donc pas mettre le month N° ".$month, E_USER_WARNING);
 			}else
-				$this->mois = $mois;
+				$this->month = $month;
 		}else{
-			trigger_error("Le mois dans la date doit etre un entier strictement positif inférieur ou égal à 12", E_USER_WARNING);
+			trigger_error("Le month dans la date doit etre un entier strictement positif inférieur ou égal à 12", E_USER_WARNING);
 		}
 	}
-	public function setAnnee($annee){
-		if(is_int($annee) AND ($annee > 1900)){
-			$this->annee = $annee;
+	public function setYear($year){
+		if(is_int($year) AND ($year > 1900)){
+			$this->year = $year;
 		}else{
-			trigger_error("Annee incorrecte", E_USER_WARNING);
+			trigger_error("year incorrecte", E_USER_WARNING);
 		}
 	}
 	// Autres finctions
@@ -82,9 +82,9 @@ class DateSys{
 	public function setDate($date){
 		$elmts = explode('-', $date);
 		if(count($elmts) == 3){
-			$this->setJour((int) $elmts[0]);
-			$this->setMois((int) $elmts[1]);
-			$this->setAnnee((int) $elmts[2]);
+			$this->setday((int) $elmts[0]);
+			$this->setmonth((int) $elmts[1]);
+			$this->setyear((int) $elmts[2]);
 		}else{
 			trigger_error("DateSys::setDate : La date est incorrect: Format (jj-mm-aaaa)", E_USER_WARNING);
 		}
@@ -96,35 +96,35 @@ class DateSys{
 	*/
 	public function getDate($param = ""){ 
 		$date = "";
-		if($this->jour < 10) $date .= "0".$this->jour;
-		else $date .= $this->jour;
+		if($this->day < 10) $date .= "0".$this->day;
+		else $date .= $this->day;
 		if($param =="l"){
-			$date .= " ".$this->getMois('l')." ";
+			$date .= " ".$this->getmonth('l')." ";
 		}else{
 			$date .= "-";
-			if($this->mois < 10) $date .= "0".$this->mois;
-			else $date .= $this->mois;
+			if($this->month < 10) $date .= "0".$this->month;
+			else $date .= $this->month;
 				$date .= "-";
 		}
-		$date .= $this->annee;
+		$date .= $this->year;
 		return $date;
 
 	}
 	/**
-		Retourne le nombre de jour du mois de l'objet
+		Retourne le nombre de day du month de l'objet
 		@param void
 		@return ..
 	*/
-	public function getNbJoursMois(){
-		return (int) date('d', mktime(0,0,0,($this->mois+1), -1, $this->annee));
+	public function getNbDaysmonth(){
+		return (int) date('d', mktime(0,0,0,($this->month+1), -1, $this->year));
 	}
 	/**
-		Retourne true si le jour est un samedi ou un dimanche
+		Retourne true si le day est un samedi ou un dimanche
 		@param $param:string
 		@return $date
 	*/
 	public function isInWeekend(){
-		return ($this->getJour('num') == 6 OR $this->getJour('num') == 7);
+		return ($this->getDay('num') == 6 OR $this->getDay('num') == 7);
 	}
 
 }
