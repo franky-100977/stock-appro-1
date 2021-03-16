cd7c850e947e952641fa90588f3893d7017fb321
franky-100977 <juniorawonofranky29@gmail.com>
2021-03-16 11:10:46 -0400

    envoie
    envoie

---
+++ view/stock-appro.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="stock.css">
+    </head>
+    <body>
+       <div class="stocks">
+           <p>stock appro</p>
+       </div> 
+       <div class="vertical-position">
+            <div class="deconnexion">
+                <h2>utilisateurs</h2>
+            </div>
+            <div class="content">
+                <p >TABLEAU DE BORD</p>
+                <div class="stock">
+                    <h1 class="heading-stock">articles</h1>
+                </div>
+                <div class="stock">
+                    <h1 class="heading-stock">sections</h1>
+                </div>
+                <div class="stock">
+                    <h1 class="heading-stock">ventes</h1>
+                </div>
+                <div class="stock">
+                    <h1 class="heading-stock">factures</h1>
+                </div>
+                <div class="stock">
+                    <h1 class="heading-stock">utilisateurs</h1>
+                </div>
+                <div class="stock">
+                    <h1 class="heading-stock">statistiques</h1>
+                </div>
+            </div>
+       </div>
+       <div class="rupture">
+            <div class="rupture1">
+                <p class="chiffre">00</p>
+                <p class="text"> articles</p>
+                <p class="text1">Plus de detail</p>
+            </div>
+            <div class="rupture2">
+                <p class="chiffre">00</p>
+                <p class="text"> en rupture de stock</p>
+                <p class="text1">plus de detail</p>
+            </div>
+            <div class="rupture3">
+                <p class="chiffre">00</p>
+                <p class="text"> veleur du stock</p>
+                <p class="text1">Plus de detail</p>
+            </div>
+       </div>
+       <div class="stat">
+            <p>statistiques</p>
+       </div>
+       <div class="last-content">
+            <div class="case">
+                <h2 class="last">Produits les plus vendus</h2>
+                <p class="last-text"> 
+                    si l'utilisateur n'est pas le gerant, afficher le
+                    tableau de bord sans le lien vers les statistiques 
+                    et les utilisateurs
+                </p>
+            </div>
+            <div class="case">
+                <h2 class="last">Evolution des ventes </h2>
+                <p class="last-text">
+                    graphe sur les 10 derniers jours
+                </p>
+            </div>
+       </div>
+       <div class="end">
+            <p class="end-text">Plus de details</p>
+       </div>
+
+    </body>
+</html>
